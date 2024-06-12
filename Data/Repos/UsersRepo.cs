d602a98742ca021ecc0e03b24aa676e5ee4e0fc5
Emil Babazade <emil.babazade@atea.com>
2024-06-12 19:23:43 +0300

    fix chunking; refactor domain entity mapping in userrepo

--- Data/Repos/UsersRepo.cs
+++ Data/Repos/UsersRepo.cs
@@ -36,15 +36,13 @@ public class UsersRepo(DataContext dataContext, IMapper mapper) : IUsersRepo
     {
         var users = await _dataContext.Users.Where(u => u.Id == id).ToListAsync(cancellation);
         _dataContext.RemoveRange(users);
-        await _dataContext.SaveChangesAsync();
+        await _dataContext.SaveChangesAsync(cancellation);
     }
 
     public async Task<User> UpdateUserAsync(User user, CancellationToken cancellationToken = default)
     {
         var userEntity = await _dataContext.Users.FirstOrDefaultAsync(u => u.Id == user.Id, cancellationToken) ?? throw new ApplicationException();
-        if (userEntity == null)
-            throw new ApplicationException("User does not exist");
-        UpdateUserEntity(user, userEntity);
+        _mapper.Map<User, UserEntity>(user, userEntity);
         await _dataContext.SaveChangesAsync(cancellationToken);
         return _mapper.Map<User>(userEntity);
     }
@@ -54,47 +52,30 @@ public class UsersRepo(DataContext dataContext, IMapper mapper) : IUsersRepo
         var userEntity = await _dataContext.Users.FirstOrDefaultAsync(u => u.Id == user.Id, cancellationToken);
         if (userEntity == null)
             return await CreateAsync(user, cancellationToken);
-        UpdateUserEntity(user, userEntity);
+        _mapper.Map<User, UserEntity>(user, userEntity);
         await _dataContext.SaveChangesAsync(cancellationToken);
         return _mapper.Map<User>(userEntity);
     }
     public async Task BulkUpsertAsync(IEnumerable<User> users, CancellationToken cancellationToken = default)
     {
-        // process in chunks of n so the entity tracking doesnt get too big
+        // process in chunks of n so the entity tracking doesn't get too big
         var chunks = users.Chunk(1000);
         foreach (var chunk in chunks)
         {
-            var existingUsers = await _dataContext.Users.Where(ue => users.Select(u => u.Id).Contains(ue.Id)).ToListAsync(cancellationToken);
+            var userIds = chunk.Select(u => u.Id);
+            var existingUserEntities = await _dataContext.Users.Where(ue => userIds.Contains(ue.Id)).ToListAsync(cancellationToken);
             // insert
-            var newUsers = users.Where(u => !existingUsers.Exists(ue => ue.Id == u.Id)).ToList();
+            var newUsers = chunk.Where(u => !existingUserEntities.Exists(ue => ue.Id == u.Id)).ToList();
             _dataContext.AddRange(_mapper.Map<List<UserEntity>>(newUsers));
             // update
-            foreach (var ue in existingUsers)
+            foreach (var ue in existingUserEntities)
             {
-                UpdateUserEntity(users.FirstOrDefault(u => u.Id == ue.Id), ue);
+                var userToUpdate = chunk.FirstOrDefault(u => u.Id == ue.Id);
+                if(userToUpdate == null) continue;
+                _mapper.Map<User, UserEntity>(userToUpdate, ue);
             }
             await _dataContext.SaveChangesAsync(cancellationToken);
             _dataContext.ChangeTracker.Clear();
         }
     }
-    private static void UpdateUserEntity(User user, UserEntity userEntity)
-    {
-        if (user == null || userEntity == null) return;
-        userEntity.Name = user.Name;
-        userEntity.Username = user.Username;
-        userEntity.Email = user.Email;
-        userEntity.StreetAddress = user.StreetAddress;
-        userEntity.ApartmentSuite = user.ApartmentSuite;
-        userEntity.City = user.City;
-        userEntity.ZipCode = user.ZipCode;
-        userEntity.Latitude = user.Latitude;
-        userEntity.Longitude = user.Longitude;
-        userEntity.Phone = user.Phone;
-        userEntity.Website = user.Website;
-        userEntity.CompanyName = user.CompanyName;
-        userEntity.CompanyCatchPhrase = user.CompanyCatchPhrase;
-        userEntity.CompanyBs = user.CompanyBs;
-    }
-
-
 }
